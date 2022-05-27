977df20b4dd55900ef39ac5b1266ade59ffd4c5c
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-26 22:17:30 -0500

    Js Solved

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,16 @@
-exports.sumPairs = function() {
+exports.sumPairs = function sumPairs(arr, num) {
 
+    let pair = []
+    let pairs = []
+    while(arr.length > 0){
+        let firstCard = arr.shift()
+        arr.forEach(function(element){
+        if(element + firstCard == num){
+            pair.push(firstCard)
+            let secondCard = element
+            pairs.push([firstCard, secondCard])
+            }
+        })
+    }
+    return pairs.length > 0 ? pairs : 'Unable to find pairs'
 };
--- js/sumPairsSpec.js
+++ js/sumPairsSpec.js
@@ -1,3 +1,6 @@
-var sp = require("./sumPairs");
+import sp from "./sumPairs";
 
-// Don't forget to add your tests :)
+console.log(sumPairs([1,2,3,4,5], 9))
+console.log(sumPairs([1,2,3,4,5], 7))
+console.log(sumPairs([3, 1, 5, 8, 2], 27))
+
